86f6e635bd96966fb7542ed86341a6e2daf04ae3
cti1650 <cti1650@gmail.com>
2021-03-07 13:34:16 +0900

    test

--- GAS/src.js
+++ GAS/src.js
@@ -1,15 +1,37 @@
 Vue.component("LinkButtons", {
   props: ["items"],
   template: `
-      <ul class="flex flex-row flex-wrap bg-gray-300">
+      <ul class="flex flex-row flex-wrap">
       <div class="w-full sm:w-1/2 lg:w-1/4 p-1" v-for="item in items">
       <a :href="item.url" target="_blank" rel="noopener noreferrer">
-      <li class="bg-white hover:shadow-inner hover:bg-gray-300 rounded-lg border border-gray-500 px-2 py-1 shadow-lg">
-      <div class="text-xs text-gray-500">{{ item.summary }}</div><div>{{ item.name }}</div>
+      <li class="bg-gray-800 hover:shadow-inner hover:bg-gray-300 rounded-lg border border-gray-600 px-2 py-1 shadow-lg text-white hover:text-black">
+      <div class="text-xs text-gray-500">{{ item.summary }}</div><div class="text-xl">{{ item.name }}</div>
       </li>
       </a>
       <div>
       </ul>
+      `,
+});
+
+Vue.component("ArticlesButtons", {
+  props: ["items"],
+  template: `
+      <ul class="flex flex-row flex-wrap">
+      <div class="w-full lg:w-1/2 p-1" v-for="item in items">
+      <a :href="item.url" target="_blank" rel="noopener noreferrer">
+      <li class="bg-gray-800 hover:shadow-inner hover:bg-gray-300 rounded-lg border border-gray-600 px-2 py-1 shadow-lg text-white hover:text-black">
+      <div class="text-xs text-gray-500">{{ item.summary }}</div><div class="text-xl">{{ item.name }}</div>
+      </li>
+      </a>
+      <div>
+      </ul>
+      `,
+});
+
+Vue.component("ListTitle", {
+  props: ["title"],
+  template: `
+      <div class="text-white mt-4 text-lg">{{title}}</div>
       `,
 });
 
@@ -27,7 +49,7 @@ Vue.component("tool", {
       var self = this;
       axios
         .get(
-          "https://script.googleusercontent.com/macros/echo?user_content_key=Vu0oc5JaKAHuhZuQB5erne5wt8CinebIfLMqz520DlExHSSul0RCQXn9GHuuQtSKkXd2_E6ZArpjlUBCfTmTs1Ku0psh1dsrm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnIpGzwPqKc1vnMAimnEk0w0dCClc-13zIEt0DBDGJKyYoaBfI-ZsUc0jPOPBMwaLOv9yq-1JZMW8j7Rhm_ixm_a9QEaL_YQ8nA&lib=MEWaq8znWJKP_ENCbnLfsT1k0Zr-bqyeo"
+          "https://script.google.com/macros/s/AKfycbw-FHf7In9FfwHy9ZjWcpU5kRz6KskqkUiK6I6FYW6ACV67llxoHiv6C_kYwFAcal-g/exec"
         )
         .then(function (res) {
           console.log(res);
@@ -36,28 +58,13 @@ Vue.component("tool", {
     },
   },
   template: `
-  <ul class="flex flex-row flex-wrap bg-gray-300">
-  <div class="w-full sm:w-1/2 lg:w-1/4 p-1" v-for="item in items.tools">
-  <a :href="item.url" target="_blank" rel="noopener noreferrer">
-  <li class="bg-white hover:shadow-inner hover:bg-gray-300 rounded-lg border border-gray-500 px-2 py-1 shadow-lg">
-  <div class="text-xs text-gray-500">{{ item.summary }}</div><div>{{ item.name }}</div>
-  </li>
-  </a>
   <div>
-  </ul>
-  <ul class="flex flex-row flex-wrap bg-gray-300">
-  <div class="w-full sm:w-1/2" v-for="item in items.modules"><a href={ item.url }>
-  <li class="bg-white hover:bg-gray-300 rounded-lg border border-gray-500 px-2 py-1">
-  <div class="text-xs text-gray-500">{{ item.summary }}</div><div>{{ item.name }}</div>
-  </li>
-  </a><div>
-  </ul>
-  <ul class="flex flex-row flex-wrap bg-gray-300">
-  <div class="w-full sm:w-1/2" v-for="item in items.articles"><a href={ item.url }>
-  <li class="bg-white hover:bg-gray-300 rounded-lg border border-gray-500 px-2 py-1">
-  <div class="text-xs text-gray-500">{{ item.summary }}</div><div>{{ item.name }}</div>
-  </li>
-  </a><div>
-  </ul>
+  <ListTitle title="ツール＆サービス" />
+  <LinkButtons :items="items.tools"></LinkButtons>
+  <ListTitle title="npm module" />
+  <LinkButtons :items="items.modules"></LinkButtons>
+  <ListTitle title="記事" />
+  <ArticlesButtons :items="items.articles"></ArticlesButtons>
+  </div>
   `,
 });
