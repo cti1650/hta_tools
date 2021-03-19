168aef549dd8a2bd098ca9f4f24e566a32ab7c95
cti1650 <cti1650@gmail.com>
2021-03-19 11:21:23 +0900

    Update TitleHeader.js

--- vue/components/TitleHeader.js
+++ vue/components/TitleHeader.js
@@ -1,5 +1,5 @@
 Vue.component('title-header', {
-  template: '<p class="border-black border-l-8 border-b my-2 py-1 pl-4 h-auto">' +
+  template: '<p class="border-black border-l-8 border-b my-2 py-1 pl-4 h-auto w-full text-xl text-left tracking-widest">' +
   '  <slot />' +
   '</p>'
 });
