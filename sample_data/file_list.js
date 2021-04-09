9d920f5c07c8d7d7922bc4b5bd7ca81c696dd91b
cti1650 <cti1650@gmail.com>
2021-04-10 06:29:03 +0900

    🌱 make file_list.js

---
+++ sample_data/file_list.js
@@ -0,0 +1,32 @@
+var json = [
+  {
+    name: "file1.name",
+    path: "file.path",
+    accessed: "dla.toString()",
+  },
+  {
+    name: "file2.name",
+    path: "file.path",
+    accessed: "dla.toString()",
+  },
+  {
+    name: "file3.name",
+    path: "file.path",
+    accessed: "dla.toString()",
+  },
+  {
+    name: "file4.name",
+    path: "file2.path",
+    accessed: "dla.toString()",
+  },
+  {
+    name: "file5.name",
+    path: "file2.path",
+    accessed: "dla.toString()",
+  },
+  {
+    name: "file6.name",
+    path: "file2.path",
+    accessed: "dla.toString()",
+  },
+];
