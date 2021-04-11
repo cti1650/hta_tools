c20c6461aec320942b3bc01009e83b4750933f8a
cti1650 <cti1650@gmail.com>
2021-04-11 22:57:21 +0900

    🌱 make LinkButton Component

---
+++ vue/components/LinkButton.js
@@ -0,0 +1,11 @@
+Vue.component("link-button", {
+  template:
+    '<div class="flex flex-col space-y-4 bg-gray-200">' +
+    " <a" +
+    '   href="https://www.google.com/"' +
+    '   class="py-2 text-black text-center border-1 border-gray-600 rounded-lg bg-gray-400 m-2"' +
+    " >" +
+    "   Google" +
+    " </a>" +
+    "</div>",
+});
